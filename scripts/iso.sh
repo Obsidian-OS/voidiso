36ec4c2c948ec91e08c4f6a0e3c7877f2e1c5293
TheOddCell <rayfb.to.1@gmail.com>
2025-10-17 19:05:23 -0400

    fix

--- scripts/iso.sh
+++ scripts/iso.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # Do not run directly from the scripts directory, do:
 # ./scripts/iso.sh
+
+cd upstream-iso
+./mkiso.sh -- \
+  -e fish \
+  -T "ObsidianOS Void Edition" \
+  -o ../latest.iso \
+  -p "$(tr '\n' ' ' < ../packages.txt)" \
+  -S "$(tr '\n' ' ' < ../services.txt)" \
+  -I ../rootfs
+cd ..
