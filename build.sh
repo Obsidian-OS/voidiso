e7a8bd8fd7b171e8863b71d0dc3dec17ade6a754
TheOddCell <rayfb.to.1@gmail.com>
2025-10-17 18:58:24 -0400

    layout and ais

--- build.sh
+++ build.sh
@@ -2,16 +2,10 @@
 # I hate makefiles dude
 
 # 1. make arch-install-scripts (and install it)
+./scripts/ais.sh
 # 2. make and install the rest of our utilities
 # 3. make sfs and install it
+./scripts/obsidianctl.sh
+./scripts/mkobsidiansfs.sh
 # 4. make iso
-cd upstream-iso
-
-# Build the ISO
-./mkiso.sh -- \
-  -e fish \
-  -T "ObsidianOS Void Edition" \
-  -o ../latest.iso \
-  -p "$(tr '\n' ' ' < ../packages.txt)" \
-  -S "$(tr '\n' ' ' < ../services.txt)" \
-  -I ../rootfs
+./scripts/iso.sh
